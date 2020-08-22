79fc200ffac5a09eb8267d1325ebc3f1c5c144fe
Wei Chin <eyoweichin@hotmail.com>
2020-08-22 13:08:06 +0800

    Level 3: Mark as Done
    Add the ability to mark tasks as done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,21 +11,31 @@ public class Duke {
         final String SINGLE_LINE =
                 "____________________________________________________________";
 
-        //Greet
+        //Greet Component
         System.out.println(SINGLE_LINE);
         System.out.println(LOGO);
         System.out.println("Hello! I'm Duke!");
         System.out.println("What can I do for you?");
         System.out.println(SINGLE_LINE);
 
-        //Interaction
+        //Interaction Component
         Scanner scanner = new Scanner(System.in);
         String userInput = "";
         boolean stillInteracting = true;
-        ArrayList<String> tasks = new ArrayList<String>();
+        ArrayList<Task> tasks = new ArrayList<Task>();
 
         while (stillInteracting) {
             userInput = scanner.nextLine();
+            String[] inputParts = userInput.split(" ");
+
+            //Check and process the 'done' command
+            int taskToFinish = 0;
+            if (inputParts[0].equals("done")) {
+                userInput = "done";
+                //Change from 0-based to 1-base indexing by deducting 1
+                taskToFinish = Integer.parseInt(inputParts[1]) - 1;
+            }
+
             switch (userInput) {
             case "bye":
                 stillInteracting = false;
@@ -35,21 +45,43 @@ public class Duke {
                     System.out.println("Fortunately, you have no tasks due");
                 } else {
                     for (int i = 0; i < tasks.size(); i++) {
-                        System.out.println(i + 1 + ": " + tasks.get(i));
+                        Task currTask = tasks.get(i);
+                        System.out.printf("%d: %s %s%n", i + 1, currTask.getTaskName(),
+                                currTask.getStatusIndicator());
+                    }
+                }
+                System.out.println(SINGLE_LINE);
+                break;
+            case "done":
+                if (taskToFinish < 0 || taskToFinish > Task.getTotalTasks()) {
+                    System.out.println("That Task does not exist!");
+                } else if (tasks.get(taskToFinish).isCompleted()) {
+                    System.out.println("That Task has already been completed, but let's shoot it again");
+                } else {
+                    //TODO: change this to else statement once tested all possible outcomes
+                    tasks.get(taskToFinish).setCompleted(true);
+                    System.out.println("Task: '" + tasks.get(taskToFinish).getTaskName() +
+                            "' marked as completed, well done!");
+                    if(Task.getTotalTasks() == 0){
+                        System.out.println("All Task Completed!");
+                    } else {
+                        System.out.println("You have " + Task.getTotalTasks() + " remaining tasks");
+                        System.out.println("We'll get them next time!");
                     }
                 }
                 System.out.println(SINGLE_LINE);
                 break;
             default:
+                Task newTask = new Task(userInput);
+                tasks.add(newTask);
                 System.out.println(SINGLE_LINE);
                 System.out.println("+ " + userInput);
                 System.out.println(SINGLE_LINE);
-                tasks.add(userInput);
                 break;
             }
         }
 
-        //Exit
+        //Exit Component
         System.out.println(SINGLE_LINE);
         System.out.println("Bye! Hope to see you again soon!");
         System.out.println(SINGLE_LINE);
---
+++ src/main/java/Task.java
@@ -0,0 +1,45 @@
+public class Task {
+    //Task attribute
+    private boolean isCompleted;
+    private String taskName;
+
+    //Class Level Attribute
+    private static int totalTasks = 0;
+
+    //Task Constructor
+    public Task(String taskName){
+        this.isCompleted = false;
+        this.taskName = taskName;
+        totalTasks += 1;
+    }
+
+    //Method of Task class
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public String getStatusIndicator() {
+        return (isCompleted ? "[✓]" : "[✗]");
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+        totalTasks -= 1;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public static int getTotalTasks() {
+        return totalTasks;
+    }
+
+    public static void setTotalTasks(int totalTasks) {
+        Task.totalTasks = totalTasks;
+    }
+}
