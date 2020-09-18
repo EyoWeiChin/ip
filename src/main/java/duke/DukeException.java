40e43c1a3a622cce000677f400b76366ee78a70f
Wei Chin <eyoweichin@hotmail.com>
2020-09-18 21:54:19 +0800

    Update DukeException to handle Duke related errors

--- src/main/java/duke/DukeException.java
+++ src/main/java/duke/DukeException.java
@@ -1,4 +1,15 @@
 package duke;
 
 public class DukeException extends Exception {
+    protected String message;
+
+    public DukeException(String message) {
+        super(message);
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "Error: " + this.message;
+    }
 }
